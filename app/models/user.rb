a1e8f986cd8da8bd33013ab5b3795e48c52c3834
Karan Parikh <karan.painkiller@gmail.com>
2010-12-27 20:20:02 +0530

    Added tests for secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :name , :presence=>true,
     :length=>{:maximum=>50}
   validates :email, :presence=>true,
@@ -22,4 +23,17 @@ class User < ActiveRecord::Base
   validates :password, :presence => true,
     :confirmation => true,
     :length       => { :within => 6..40 }
+  before_save :encrypt_password
+
+  def has_password?(submitted_password)
+  end
+
+  private
+    def encrypt_password
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      string
+    end
 end
