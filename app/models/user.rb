652d8f474c5665497221cb80be0f9d6ac04997c7
Karan Parikh <karan.painkiller@gmail.com>
2010-12-27 22:51:49 +0530

    added secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,18 @@
 # == Schema Information
-# Schema version: 20101226093425
+# Schema version: 20101227170945
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
+require 'digest'
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
@@ -26,14 +29,24 @@ class User < ActiveRecord::Base
   before_save :encrypt_password
 
   def has_password?(submitted_password)
+    encrypt_password == encrypt(submitted_password)
   end
 
   private
     def encrypt_password
+      self.salt = make_salt if new_record?
       self.encrypted_password = encrypt(password)
     end
 
     def encrypt(string)
-      string
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
     end
 end
