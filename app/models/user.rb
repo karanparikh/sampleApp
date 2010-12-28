f41764eb1af15672d7b3fcbd1a9c0978e63820b5
Karan Parikh <karan.painkiller@gmail.com>
2010-12-28 11:27:53 +0530

    User model is complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,13 @@ class User < ActiveRecord::Base
   before_save :encrypt_password
 
   def has_password?(submitted_password)
-    encrypt_password == encrypt(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
   end
 
   private
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -107,5 +107,23 @@ describe User do
         @user.has_password?("invalid").should be_false
       end
     end
+
+    describe "authenticate method" do
+
+      it "should return nil on email/password mismatch" do
+        wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
+        wrong_password_user.should be_nil
+      end
+
+      it "should return nil for an email address with no user" do
+        nonexistant_user = User.authenticate("aar@eoo.com", @attr[:password])
+        nonexistant_user.should be_nil
+      end
+
+      it "should return the user on email and password match" do
+        matching_user = User.authenticate(@attr[:email], @attr[:password])
+        matching_user.should == @user
+      end
+    end
   end
 end
