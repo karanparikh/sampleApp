f503db5023be230133a98dbe16f4e59f087dfbb5
Karan Parikh <karan.painkiller@gmail.com>
2011-01-06 12:06:07 +0530

    Added a basic sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 SampleApp::Application.routes.draw do
+
+  resources :sessions, :only => [:new, :create, :destroy]
   resources :users
+
   match '/contact', :to=> 'pages#contact'
   match '/about',   :to=> 'pages#about'
   match '/help',    :to=> 'pages#help'
   match '/signup',  :to=> 'users#new'
+
   root :to=>'pages#home'
+
+  match '/signin',  :to=> 'sessions#new'
+  match '/signout', :to=> 'sessions#destroy'
 end
