cf4dd8d629c0071eca8a09a67d8d5f2cf538d4a3
Karan Parikh <karan.painkiller@gmail.com>
2011-01-27 23:25:02 -0600

    basic user authentication and page protection complete

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,8 +5,13 @@ module SessionsHelper
   end
 
   def deny_access
-    flash[:notice] = "Please sign in to access this page."
-    redirect_to signin_path
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page"
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
   end
 
   def sign_in(user)
@@ -33,6 +38,14 @@ module SessionsHelper
 
   private
     
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
+    end
+
     def user_from_remember_token
       User.authenticate_with_salt(*remember_token)
     end
