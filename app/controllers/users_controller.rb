fada2951bff5fb71ad00de89c407008428663974
Karan Parikh <karan.painkiller@gmail.com>
2011-01-29 20:24:19 -0600

    wrote tests for index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :only => [:edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
+
+  def index
+    @title = "All users"
+    @users = User.all
+  end
+
   def new
     @user  = User.new
     @title = "Sign up"
